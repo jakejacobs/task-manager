f8168cf00895303a4d169d170cb4ab22f910d4b3
jakejacobs <jacky4uonly2009@gmail.com>
2021-07-07 20:37:50 +0530

    login comment test

--- src/routers/user.js
+++ src/routers/user.js
@@ -14,6 +14,7 @@ router.post('/users', async (req, res) => {
 	}
 });
 
+//login
 router.post('/users/login', async (req, res) => {
 	try {
 		const user = await User.findByCredentials(req.body.email, req.body.password);
